18a93384ac12b78c3f8aed5a1ea508d9535b4871
Máximo Zarza <zmaximo1990@gmail.com>
2018-12-04 20:21:08 -0300

    Check permissions to post a comment

--- api/podcasts/permissions.py
+++ api/podcasts/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 
 class IAmMe(BasePermission):
     def has_permission(self, request, view):
-        if request.method != 'POST':
+        if request.method != 'POST' and request.method != 'GET':
             return False
         if (request.method == 'POST'):
             return request.data.get('user') == request.user.id
